afeb3883ca4d9a4f36ae6547caf93c2c7bd36ff5
Murillo Silva <mfs.murillo@gmail.com>
2021-11-13 19:00:07 -0300

    v1.0

--- index.php
+++ index.php
@@ -8,24 +8,22 @@
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1" />
         <title><?php echo $title; ?></title>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $pageurl; ?>/dist/assets/images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="60x60" href="<?php echo $pageurl; ?>/dist/assets/images/favicon.png" />
-        <link rel="manifest" href="<?php echo $pageurl; ?>/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="60x60" href="<?php echo $pageurl; ?>/public/images/favicon.png" />
         
         <meta name="msapplication-TileColor" content="#111111" />
         <meta name="theme-color" content="#111111" />
 
-        <link rel="stylesheet" href="<?php echo $pageurl; ?>/dist/main.css" />
+        <link rel="stylesheet" href="<?php echo $pageurl; ?>/public/index.css" />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content="<?php echo $title; ?>" />
         <meta property="og:description" content="<?php echo $description; ?>" />
-        <meta property="og:image" content="<?php echo $pageurl; ?>/dist/images/share.jpg" />
+        <meta property="og:image" content="<?php echo $pageurl; ?>/public/images/share.jpg" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="<?php echo $title; ?>" />
         <meta name="twitter:description" content="<?php echo $description; ?>" />
-        <meta name="twitter:image" content="<?php echo $pageurl; ?>/dist/images/share.jpg" />
+        <meta name="twitter:image" content="<?php echo $pageurl; ?>/public/images/share.jpg" />
     </head>
 
     <body id="<?php echo $page; ?>">
@@ -49,6 +47,6 @@
             </div>
         </div>
         
-        <script src="<?php echo $pageurl; ?>/dist/main.bundle.js"></script>
+        <script src="<?php echo $pageurl; ?>/public/index.js"></script>
     </body>
 </html>
